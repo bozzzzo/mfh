c8aad08041e9386a518700dcf10eb92071b8f13c
Bozo Dragojevic <bozzo@digiverse.si>
2019-12-04 13:02:56 +0100

    Piece movements

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,6 +1,129 @@
 module Lib
     ( someFunc
+    , Space (..)
+    , Piece (..)
+    , SpaceSize
+    , empty
+    , make
+    , sparse
+    , canonic
+    , piece
+    , size
+    , a
+    , shadow
+    , rotx
+    , roty
+    , rotz
+    , rotations
+    , move
+    , moves
+    , flail
     ) where
+
+import Data.List
 
 someFunc :: IO ()
 someFunc = putStrLn "someFunc"
+
+type SpaceSize = [Int]
+data  Space = Space' SpaceSize [[[String]]] deriving (Eq, Ord)
+
+instance Show Space where
+  show = showSpace ""
+
+showSpace :: String -> Space -> String
+showSpace p (Space' [_,_,0] [ys]) = "\n" ++ concat [concat [p, "|", concat [padtwo x ++ " "| x <- xs], "|\n"] | xs <- ys]
+showSpace p (Space' [x,y,z] (ys:zs)) = concat [showSpace p (Space' [x,y,0] [ys]),
+    p ++ " " ++ concat (replicate (y+1) "--"),
+    showSpace (p ++ " ") (Space' [x,y,z-1] zs)]
+
+padtwo :: String -> String
+padtwo [x] = [x] ++ " "
+padtwo x = x
+
+empty :: [Int] -> Space
+empty [x,y] = Space' [x-1,y-1,0] [replicate y b]
+  where b = replicate x " "
+empty [x,y,z] = Space' [x-1,y-1,z-1] $ replicate z p
+  where Space' _ [p] = empty [x, y]
+
+data Piece = Piece' String [(Int,Int,Int)] deriving (Eq, Ord)
+
+instance Show Piece where
+  show = show . make
+ 
+a = make (Piece' "A"  [(0,0,0),(0,1,0),(1,0,0),(1,1,0)])
+
+size :: Piece -> SpaceSize
+size (Piece' _ xyzs) = [foldl max 0 (map (st n) xyzs) | n <- [0..2]]
+
+st :: Int -> (Int, Int, Int) -> Int
+st 0 (x, _, _) = x
+st 1 (_, y, _) = y
+st 2 (_, _, z) = z
+
+canonic :: Piece -> Piece
+canonic (Piece' n xyzs) = Piece' n (sort xyzs)
+
+piece :: String -> [(Int, Int, Int)] -> Piece
+piece n xyzs = canonic $ Piece' n xyzs
+
+make :: Piece -> Space
+make p = Space' d [[[c (x,y,z) | x <- [0..xs]] | y <- [0..ys]] | z <- [0..zs]]
+  where d = size p
+        (Piece' n xyzs) = p
+        [xs,ys,zs] = d
+        c = \e -> if e `elem` xyzs then n else " "
+
+
+sparse :: Space -> Piece
+sparse (Space' [xs,ys,zs] s) = piece n xyzs
+  where n = head . (filter (/= " ")) . nub $ concat $ concat s
+        xyzs = [(i,j,k) | (z,k) <- zip s [0..zs]
+                        , (y,j) <- zip z [0..ys]
+                        , (x,i) <- zip y [0..xs]
+                        , x == n]
+
+
+shadow :: Piece -> Piece
+shadow (Piece' n xyzs) = piece (n++"'") [(x,y,z-1)
+                                        | (x,y,z) <- xyzs,
+                                          z > 0, 
+                                          not ((x,y,z-1) `elem` xyzs)]
+
+rotx :: Piece -> Piece
+rotx p = piece n [(x,z,ys-y) | (x,y,z) <- xyzs]
+  where [_, ys, _] = size p
+        Piece' n xyzs = p
+
+roty :: Piece -> Piece
+roty p = piece n [(zs-z,y,x) | (x,y,z) <- xyzs]
+  where [_, _, zs] = size p
+        Piece' n xyzs = p
+
+rotz :: Piece -> Piece
+rotz p = piece n [(ys-y,x,z) | (x,y,z) <- xyzs]
+  where [_, ys, _] = size p
+        Piece' n xyzs = p
+
+
+rotations :: Piece -> [Piece]
+rotations p = sort . nub $ concat [ take 4 $ iterate rotx b | b <- concat [take 4 $ iterate roty a | a <- take 4 $ iterate rotz p]]
+
+
+move :: (Int,Int,Int) -> Piece -> Piece
+move (dx,dy,dz) (Piece' n xyzs) = piece n [(x+dx,y+dy,z+dz) | (x,y,z) <- xyzs]
+
+moves :: SpaceSize -> Piece -> [Piece]
+moves [sx,sy,sz] p = [move (dx,dy,dz) p
+                     | dx <- [0..sx-px],
+                       dy <- [0..sy-py],
+                       dz <- [0..sz-pz],
+                       px <= sx,
+                       py <= sy,
+                       pz <= sz]
+  where
+    [px, py, pz] = size p
+
+flail :: SpaceSize -> Piece -> [Piece]
+flail s p = concatMap (moves s) (rotations p)
--- test/Spec.hs
+++ test/Spec.hs
@@ -1,2 +1,97 @@
+import Test.Hspec
+import Test.QuickCheck
+import Data.List
+import Lib
+
+dot = piece "x" [(0,0,0)]
+
 main :: IO ()
-main = putStrLn "Test suite not yet implemented"
+main = hspec $ do
+  describe "empty" $ do
+     it "can be 2D 1x1" $ do
+       empty [1,1] `shouldBe` Space' [0,0,0] [[[" "]]]
+     it "can be 2D 1x2" $ do
+       empty [1,2] `shouldBe` Space' [0,1,0] [[[" "]
+                                             ,[" "]]]
+     it "can be 3D 1x1x1" $ do
+       empty [1,1,1] `shouldBe` Space' [0,0,0] [[[" "]]]
+
+     it "can be 3D 1x2x3" $ do
+       empty [1,2,3] `shouldBe` Space' [0,1,2] [[[" "]
+                                               ,[" "]]
+                                              ,[[" "]
+                                               ,[" "]]
+                                              ,[[" "]
+                                               ,[" "]]]
+
+  describe "rotations" $ do
+    let p = piece "x" [(0,0,0),(1,0,0),(0,2,1)]
+    describe "rotz" $ do
+      it "rotates clockwise" $ do
+        rotz p `shouldBe` piece "x" [(2,0,0),(2,1,0),(0,0,1)]
+      it "comes around" $ do
+        (rotz . rotz . rotz . rotz) p `shouldBe` p
+    describe "roty" $ do
+      it "rotates clockwise" $ do
+        roty p `shouldBe` piece "x" [(1,0,0),(1,0,1),(0,2,0)]
+      it "comes around" $ do
+        (roty . roty . roty . roty) p `shouldBe` p
+    describe "rotx" $ do
+      it "rotates clockwise" $ do
+        rotx p `shouldBe` piece "x" [(0,0,2),(1,0,2),(0,1,0)]
+      it "comes around" $ do
+        (rotx . rotx . rotx . rotx) p `shouldBe` p
+    describe "rotations" $ do
+      it "leaves dot alone" $ do
+        rotations dot  `shouldBe` [dot]
+      it "has 3 images for stick" $ do
+        let stick = piece "x" [(0,0,0),(0,1,0)]
+        rotations stick `shouldBe` sort [rotx stick, roty stick, rotz stick]
+
+  describe "shadow" $ do
+    it "has no shadow under dot" $ do
+      shadow dot `shouldBe` piece "x'" []
+    it "has shadow under raised dot" $ do
+      let r = piece "x" [(0,0,1)]
+      shadow r `shouldBe` piece "x'" [(0,0,0)]
+    it "has shadow under raised stick" $ do
+      let r = piece "x" [(0,0,1),(0,0,2)]
+      shadow r `shouldBe` piece "x'" [(0,0,0)]
+    it "has shadow under z" $ do
+      let z = piece "z" [(0,0,1),(1,0,1),(1,0,0),(2,0,0)]
+      shadow z `shouldBe` piece "z'" [(0,0,0)]
+    it "has shadow under raised z" $ do
+      let z = piece "z" [(0,0,3),(0,1,3),(0,1,2),(0,2,2)]
+      shadow z `shouldBe` piece "z'" [(0,0,2),(0,1,1),(0,2,1)]
+
+  describe "move" $ do
+    it "can do nothing" $ do
+      move (0,0,0) dot `shouldBe` dot
+    it "can move in x" $ do
+      move (1,0,0) dot `shouldBe` piece "x" [(1,0,0)]
+    it "can move in y" $ do
+      move (0,1,0) dot `shouldBe` piece "x" [(0,1,0)]
+    it "can move in z" $ do
+      move (0,0,1) dot `shouldBe` piece "x" [(0,0,1)]
+    it "can move in xyz" $ do
+      move (3,2,1) dot `shouldBe` piece "x" [(3,2,1)]
+
+  describe "moves" $ do
+    it "can do nothing" $ do
+      moves [0,0,0] dot `shouldBe` [dot]
+    it "can move dot" $ do
+      sort (moves [1,1,1] dot) `shouldBe` sort [ move (x,y,z) dot
+                                                 | x <- [0..1],
+                                                   y <- [0..1],
+                                                   z <- [0..1]]
+
+  describe "flail" $ do
+    it "can do nothing" $ do
+      flail [0,0,0] dot `shouldBe` [dot]
+    it "can move dot" $ do
+      flail [1,1,1] dot `shouldBe` moves [1,1,1] dot
+    it "can move stick" $ do
+      let stick = piece "x" [(0,0,0),(0,1,0)]
+          s = [2,2,2]
+      sort (flail s stick) `shouldBe` (sort $ concatMap (moves s) [
+        stick, (rotx stick), (rotz stick)])
